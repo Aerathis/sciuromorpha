faa52a76993d3e10b8150518a164a4caaea32d6a
Jeff <jeffrey@ripostegames.com>
2017-03-27 18:48:46 +0000

    Updates for fetch options

--- lib/gitclient.go
+++ lib/gitclient.go
@@ -1,6 +1,9 @@
 package sciuromorpha
 
 import (
+	"errors"
+	dirpath "path"
+
 	git "github.com/libgit2/git2go"
 )
 
@@ -11,16 +14,18 @@ var checkoutOpts = &git.CheckoutOpts{
 // GitClient manages a reference to a git repository on disk
 type GitClient struct {
 	repository *git.Repository
+	sshpath    string
 }
 
 // OpenRepository opens a reference to a git repository at the given path
-func OpenRepository(path string) (gc *GitClient, err error) {
+func OpenRepository(path, sshpath string) (gc *GitClient, err error) {
 	repo, err := git.OpenRepository(path)
 	if err != nil {
 		return nil, err
 	}
 	gc = &GitClient{}
 	gc.repository = repo
+	gc.sshpath = sshpath
 	return
 }
 
@@ -29,15 +34,40 @@ func (gc *GitClient) Free() {
 	gc.repository.Free()
 }
 
+func getFetchOpts(gc *GitClient) *git.FetchOptions {
+	return &git.FetchOptions{
+		RemoteCallbacks: git.RemoteCallbacks{
+			CertificateCheckCallback: func(*git.Certificate, bool, string) git.ErrorCode {
+				return git.ErrOk
+			},
+			CredentialsCallback: func(string, string, git.CredType) (git.ErrorCode, *git.Cred) {
+				ret, cred := git.NewCredSshKey("git", dirpath.Join(gc.sshpath, "id_rsa.pub"), dirpath.Join(gc.sshpath, "id_rsa"), "")
+				return git.ErrorCode(ret), &cred
+			},
+		},
+	}
+}
+
 // CheckoutTag instructs the git client to checkout the provided tag onto disk from the repository
 func (gc *GitClient) CheckoutTag(tag string) (err error) {
+	r, err := gc.repository.Remotes.Lookup("origin")
+	if err != nil {
+		return err
+	}
+	defer r.Free()
+
+	err = r.Fetch([]string{"+refs/heads/*:refs/remotes/origin/*", "refs/tags/*:refs/tags/*"}, getFetchOpts(gc), "")
+	if err != nil {
+		return err
+	}
+
 	odb, err := gc.repository.Odb()
 	if err != nil {
 		return err
 	}
 	defer odb.Free()
 
-	t := &git.Tag{}
+	var t *git.Tag
 	odb.ForEach(func(oid *git.Oid) error {
 		obj, err := gc.repository.Lookup(oid)
 		if err != nil {
@@ -51,7 +81,11 @@ func (gc *GitClient) CheckoutTag(tag string) (err error) {
 		}
 		return nil
 	})
-	defer t.Free()
+	if t != nil {
+		defer t.Free()
+	} else {
+		return errors.New("Unabled to find specified tag")
+	}
 
 	tagCommit, err := t.Target().AsCommit()
 	if err != nil {
--- sciuromorpha.go
+++ sciuromorpha.go
@@ -9,16 +9,20 @@ import (
 
 var tag = flag.String("tag", "", "Git tag to checkout")
 var repoPath = flag.String("repopath", "", "Absolute path to reposoitory")
+var sshPath = flag.String("sshpath", "", "Path to ssh credentials for remote git server")
 
 func main() {
 	flag.Parse()
 	fmt.Println("Searching", *repoPath, "for git tag", *tag)
 
-	repo, err := git.OpenRepository(*repoPath)
+	repo, err := git.OpenRepository(*repoPath, *sshPath)
 	if err != nil {
 		panic(err)
 	}
 	defer repo.Free()
 
-	repo.CheckoutTag(*tag)
+	err = repo.CheckoutTag(*tag)
+	if err != nil {
+		panic(err)
+	}
 }
